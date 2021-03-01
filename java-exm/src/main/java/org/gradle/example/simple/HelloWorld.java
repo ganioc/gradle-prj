34172c65be909e055c867a07eda4952c064e459f
yango3 <o3.yang@hotmail.com>
2021-03-01 16:21:17 +0800

    1st commit

---
+++ java-exm/src/main/java/org/gradle/example/simple/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.gradle.example.simple;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("hello world.");
+        System.out.println("NO");
+    }
+}
